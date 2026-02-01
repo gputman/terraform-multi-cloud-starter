3b87578687f14dc9c40ec4cd3e3c22bb24fc4452
Gregory Putman <gregoryputman43@hotmail.com>
2026-02-01 03:23:35 -0700

    Create variables.tf

---
+++ envs/aws-dev/envs/aws-dev/envs/aws-dev/envs/aws-dev/modules/aws-vpc/modules/aws-vpc/modules/aws-vpc/modules/aws-ec2/modules/aws-ec2/modules/aws-ec2/envs/azure-dev/envs/azure-dev/envs/azure-dev/envs/azure-dev/modules/azure-network/modules/azure-network/modules/azure-network/modules/azure-vm/variables.tf
@@ -0,0 +1,6 @@
+variable "project_name" { type = string }
+variable "location" { type = string }
+variable "rg_name" { type = string }
+variable "subnet_id" { type = string }
+variable "admin_username" { type = string }
+variable "public_key_path" { type = string }
