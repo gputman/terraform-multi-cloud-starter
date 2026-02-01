2bdbff2ccbfe56518624490044e39493165d63e8
Gregory Putman <gregoryputman43@hotmail.com>
2026-02-01 02:58:23 -0700

    Create provider.tf

---
+++ envs/aws-dev/provider.tf
@@ -0,0 +1,16 @@
+envs/aws-dev/provider.tf
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
