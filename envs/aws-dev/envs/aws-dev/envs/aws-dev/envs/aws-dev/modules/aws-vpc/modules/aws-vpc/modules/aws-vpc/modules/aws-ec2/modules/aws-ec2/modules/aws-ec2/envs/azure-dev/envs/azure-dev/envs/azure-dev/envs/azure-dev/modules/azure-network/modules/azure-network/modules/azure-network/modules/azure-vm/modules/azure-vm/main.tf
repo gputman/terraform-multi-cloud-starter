b6fdc2077f73f23baed001d48c294d35f41742b9
Gregory Putman <gregoryputman43@hotmail.com>
2026-02-01 03:24:22 -0700

    Create main.tf

---
+++ envs/aws-dev/envs/aws-dev/envs/aws-dev/envs/aws-dev/modules/aws-vpc/modules/aws-vpc/modules/aws-vpc/modules/aws-ec2/modules/aws-ec2/modules/aws-ec2/envs/azure-dev/envs/azure-dev/envs/azure-dev/envs/azure-dev/modules/azure-network/modules/azure-network/modules/azure-network/modules/azure-vm/modules/azure-vm/main.tf
@@ -0,0 +1,49 @@
+resource "azurerm_public_ip" "this" {
+  name                = "${var.project_name}-pip"
+  location            = var.location
+  resource_group_name = var.rg_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "this" {
+  name                = "${var.project_name}-nic"
+  location            = var.location
+  resource_group_name = var.rg_name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.this.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "this" {
+  name                = "${var.project_name}-vm"
+  location            = var.location
+  resource_group_name = var.rg_name
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.this.id
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
